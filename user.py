caf67732d7dadd308924769eab1f8f8cf41a15f4
opanga77 <andersoking77@gmail.com>
2020-05-10 07:14:01 +0300

    first commit

---
+++ user.py
@@ -0,0 +1,10 @@
+class Users
+user_list = []
+
+def __init__(self.user_name,pasword)
+    self.user_name = user_naame
+    self.password = password
+
+
+def save_user(self):
+    User.user_list.append(self)
